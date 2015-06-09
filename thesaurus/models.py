c6f4013f6e4c18143af7069e963d896af16218cd
Sviatoslav Abakumov <abakumov.sviatoslav@progforce.com>
2015-06-10 00:27:08 +0300

    Rename field 'synonym' to 'synonyms' [2]

--- thesaurus/models.py
+++ thesaurus/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Descriptor(models.Model):
     name = models.CharField(max_length=256)
     description = models.TextField(null=True, blank=True)
-    synonym = models.ManyToManyField('self', blank=True)
+    synonyms = models.ManyToManyField('self', blank=True)
     parents = models.ManyToManyField('self', related_name='children', symmetrical=False, blank=True)
     higher_level_descriptors = models.ManyToManyField('self', related_name='lower_level_descriptors', symmetrical=False, blank=True)
     part_of = models.ManyToManyField('self', related_name='consists_of', symmetrical=False, blank=True)
