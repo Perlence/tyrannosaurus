3b3fe51412b23daf54849511f53ffae695d84bfe
Sviatoslav Abakumov <abakumov.sviatoslav@progforce.com>
2015-06-10 00:29:48 +0300

    Add view to search descriptors

--- thesaurus/views.py
+++ thesaurus/views.py
@@ -1,3 +1,20 @@
+from django.db.models import Q
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Descriptor
+
+
+def query(request):
+    q = request.GET.get('q')
+    if not q:
+        return render(request, 'index.html')
+
+    descriptors = (Descriptor.objects
+                   .filter(Q(name__icontains=q) | Q(description__icontains=q))
+                   .prefetch_related('synonyms',
+                                     'parents', 'children',
+                                     'higher_level_descriptors', 'lower_level_descriptors',
+                                     'part_of', 'consists_of',
+                                     'associated_with',
+                                     'related_technologically'))
+    return render(request, 'index.html', {'descriptors': descriptors})
