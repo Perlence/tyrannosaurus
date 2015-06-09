ce9458e3c7c5d8351fa25ca273893dec848934ac
Sviatoslav Abakumov <abakumov.sviatoslav@progforce.com>
2015-06-09 21:36:20 +0300

    Initial commit

---
+++ thesaurus/migrations/0001_initial.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Descriptor',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=256)),
+                ('description', models.TextField(null=True, blank=True)),
+                ('associated_with', models.ManyToManyField(related_name='associated_with_rel_+', to='thesaurus.Descriptor', blank=True)),
+                ('consists_of', models.ManyToManyField(related_name='consists_of_rel_+', to='thesaurus.Descriptor', blank=True)),
+                ('higher_level_descriptors', models.ManyToManyField(related_name='higher_level_descriptors_rel_+', to='thesaurus.Descriptor', blank=True)),
+                ('parents', models.ManyToManyField(related_name='children', to='thesaurus.Descriptor', blank=True)),
+                ('related_technologically', models.ManyToManyField(related_name='related_technologically_rel_+', to='thesaurus.Descriptor', blank=True)),
+                ('synonym', models.ManyToManyField(related_name='synonym_rel_+', to='thesaurus.Descriptor', blank=True)),
+            ],
+        ),
+    ]
---
+++ thesaurus/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+class Descriptor(models.Model):
+    name = models.CharField(max_length=256)
+    description = models.TextField(null=True, blank=True)
+    synonym = models.ManyToManyField('self', blank=True)
+    parents = models.ManyToManyField('self', related_name='children', symmetrical=False, blank=True)
+    higher_level_descriptors = models.ManyToManyField('self', related_name='lower_level_descriptors', blank=True)
+    consists_of = models.ManyToManyField('self', related_name='part_of', blank=True)
+    associated_with = models.ManyToManyField('self', blank=True)
+    related_technologically = models.ManyToManyField('self', blank=True)
+
+    def __unicode__(self):
+        return unicode(self.name)
